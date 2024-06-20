9cf5f8a87ec39e5e721d2ced79ce7dbf88e0d45a
Ashish Gangwar <104550769+Ashish19112001@users.noreply.github.com>
2024-06-20 19:02:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+
+var typed = new Typed('#element', {
+    strings: ['Frontend Developer','Web Developer', 'Photo Editor', 'Video Editor'],
+    typeSpeed:50,
+    backSpeed:40,
+    backDelay:500,
+    loop:true
+  });
